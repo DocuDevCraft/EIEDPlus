b3f2d41a55cd7d1cd26ac63904f450d9023dbb11
Ehsan <ehsaninpublic@gmail.com>
2025-12-14 12:04:33 +0330

    Upload

--- REV05/Modules/UserAccessHandler/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('useraccesshandler::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('useraccesshandler.name') !!}
-    </p>
-@endsection
